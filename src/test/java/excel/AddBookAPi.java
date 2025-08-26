604288d9e516513637ccfba3809d4227edb7bdea
arjunc <arjunc@amdocs.com>
2025-08-26 11:04:37 +0530

    HashMapUpdate

--- src/test/java/excel/AddBookAPi.java
+++ src/test/java/excel/AddBookAPi.java
@@ -4,6 +4,8 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import static io.restassured.RestAssured.*;
 
+import java.util.HashMap;
+
 import org.testng.annotations.Test;
 
 
@@ -12,15 +14,17 @@ public class AddBookAPi {
 	@Test
 	public void addBook() {
 		
+		
+		HashMap<String, Object> JsonAsMap = new HashMap<>();
+		JsonAsMap.put("name", "Learn Appium Automation with Java");
+		JsonAsMap.put("isbn", "bcds");
+		JsonAsMap.put("aisle", "2393");
+		JsonAsMap.put("author", "John foer");
+		
 		RestAssured.baseURI="https://rahulshettyacademy.com";
 		
-		String resp = given().body("{\r\n"
-				+ "\"name\":\"Learn Appium Automation with Java\",\r\n"
-				+ "\"isbn\":\"bcd\",\r\n"
-				+ "\"aisle\":\"2926\",\r\n"
-				+ "\"author\":\"John foer\"\r\n"
-				+ "}\r\n"
-				+ "")
+		String resp = given().header("Content-Type", "application/json")
+				.body(JsonAsMap)
 			.when().log().all().post("Library/Addbook.php").then().extract().response().asString();
 		
 		JsonPath js = new JsonPath(resp);
